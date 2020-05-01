952295a4a349a8a1e7e4b6ba2a3d07ba6dd2a587
[yair-r] <yair7705778@gmail.com>
2020-05-01 15:10:51 +0300

    222

--- js/main.js
+++ js/main.js
@@ -4,133 +4,121 @@ function start() {
   mooveSmile();
 }
 
-// функция запроса на старт
-wapka.onclick = function () {
+// function ready to start
+button_header.onclick = function () {
   // var isStart222 = confirm("Lets Start ?");
   // if (isStart222 == true) {
   start();
   // }
 };
 
-
-// функция для вычесления рандомного числа в заданных границах
+// function for the calculated numbers of randomly within certain limits
 function randomInteger(min, max) {
-  // получить случайное число от (min-0.5) до (max+0.5)
+  // get a random number from (min-0.5) up to (max+0.5)
   let rand = min - 0.5 + Math.random() * (max - min + 1);
   return Math.round(rand);
 }
 
 function mooveSmile() {
-  var smile = document.getElementById('smile');
-  // функция перемещения смайла через 2секунды после наведения на него 
-  smile.onmouseover = function (e) { // 1. отследить наведение на смайл
+  var smile = document.getElementById("smile");
+  // the function of moving the smile 2 seconds after hovering over it
+  smile.onmouseover = function (e) {
+    // 1. track the hover on the smile
     intervalID = setTimeout(function () {
-      smile.style.position = 'absolute';
+      smile.style.position = "absolute";
       moveAt(e);
 
       function moveAt(e) {
-        var ekran = document.getElementById('ekran');
+        var screen = document.getElementById("screen");
 
-        smile.style.left = randomInteger(1, ekran.clientWidth - smile.clientWidth) + 'px';
-        smile.style.top = randomInteger(1, ekran.clientHeight - smile.clientWidth) + 'px';
-
+        smile.style.left =
+          randomInteger(1, screen.clientWidth - smile.clientWidth) + "px";
+        smile.style.top =
+          randomInteger(1, screen.clientHeight - smile.clientWidth) + "px";
       }
-    }, 2000)
-  }
+    }, 2000);
+  };
 }
 
-
-
-var intervalID; // должна висеть тут ибо ее использует функция stop
-// функция таймер
-
+var intervalID; // should hang here because it is used by the stop function
+// function timer
 
 var milSec = 99;
 var sec = 19;
 
 function timerF() {
-
-
-
-  timer = document.getElementById('timer');
+  timer = document.getElementById("timer");
 
   intervalID = setInterval(function () {
-
     milSec--;
 
-    if (sec == 0 & milSec == 0) {
+    if ((sec == 0) & (milSec == 0)) {
       alert("time out");
       clearInterval(intervalID);
-    };
+    }
 
     // if ((milSec == 0 & Sec !== 0)) {
-    if (milSec == 0 & sec > 0) {
+    if ((milSec == 0) & (sec > 0)) {
       sec--;
 
       milSec = 99;
-    };
+    }
 
     if (sec <= 10) {
       timer.style.color = "red";
-    };
+    }
 
-    if (milSec < 10 & sec < 10) {
-      timer.innerText = "0" + sec + " : " + "0" + milSec
-    };
+    if ((milSec < 10) & (sec < 10)) {
+      timer.innerText = "0" + sec + " : " + "0" + milSec;
+    }
 
-    if (milSec >= 10 & sec < 10) {
-      timer.innerText = "0" + sec + " : " + milSec
-    };
+    if ((milSec >= 10) & (sec < 10)) {
+      timer.innerText = "0" + sec + " : " + milSec;
+    }
 
-    if (milSec < 10 & sec >= 10) {
-      timer.innerText = sec + " : " + "0" + milSec
-    };
+    if ((milSec < 10) & (sec >= 10)) {
+      timer.innerText = sec + " : " + "0" + milSec;
+    }
 
-    if (milSec >= 10 & sec >= 10) {
-      timer.innerText = sec + " : " + milSec
-    };
-
-  
-
+    if ((milSec >= 10) & (sec >= 10)) {
+      timer.innerText = sec + " : " + milSec;
+    }
   }, 10);
-
-
-
 }
 
-
-
-// подсчет кликов по цели и мимо
+// counting clicks on and past a goal
 function summClick() {
-  var scoreN = document.getElementById('scoreN');
+  var scoreN = document.getElementById("scoreN");
   var scoreSum = 0;
   // var inc = 0;
-  var levelN = document.getElementById('levelN');
+  var levelN = document.getElementById("levelN");
   var levelNel = 1;
-  var pnl = document.getElementById('pnl');
+  var pnl = document.getElementById("pnl");
   pnlE = 10; // points to Next Level
 
-  smile.onclick = function myFunction() { //счетчик попаданий
+  smile.onclick = function myFunction() {
+    //hit counter
 
-    scoreSum = scoreSum + (levelNel * 10);
-    pnlE--; //уменьшаем количество кликов до след.уровня
+    scoreSum = scoreSum + levelNel * 10;
+    pnlE--; //reducing the number of clicks to the next level
 
-    if (pnlE == 0) { //повышаем уровень
-      pnlE = 10
+    if (pnlE == 0) {
+      //raising the level
+      pnlE = 10;
       levelNel++;
-    };
+    }
 
     scoreSum = scoreSum + levelNel;
     scoreN.innerText = scoreSum;
     levelN.innerText = levelNel;
     pnl.innerText = pnlE;
     inc2--;
-  }
-  var inc2 = 1; //счетчик промахов
-  ekran.onclick = function myFunction2() {
+  };
+  var inc2 = 1; //a counter of misses
+  screen.onclick = function myFunction2() {
     missN.innerText = inc2++;
 
     scoreSum = scoreSum - levelNel;
     scoreN.innerText = scoreSum;
-  }
+  };
 }
